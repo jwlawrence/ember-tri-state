d9e4e3c288eebf2aaf3f0ef07c20d29fbcb7f029
Joshua Lawrence <jlawrence@linkedin.com>
2017-03-15 21:17:06 -0700

    Leveraged PromiseProxyMixin to derive promise state

--- addon/components/tri-state.js
+++ addon/components/tri-state.js
@@ -1,30 +1,36 @@
 import Ember from 'ember';
 import layout from 'ember-tri-state/templates/components/tri-state';
 
-const { Component, isEmpty, isPresent, RSVP, run } = Ember;
+const {
+  Component,
+  computed,
+  isPresent,
+  K: noop,
+  Object: EmberObject,
+  PromiseProxyMixin,
+  RSVP,
+} = Ember;
 
-const STATE = Object.freeze({
-  NONE: 'NONE',
-  LOADING: 'LOADING',
-  RESOLVED: 'RESOLVED',
-  REJECTED: 'REJECTED',
-});
+const PromiseObject = EmberObject.extend(PromiseProxyMixin);
 
 /**
- * The purpose of this component is to handle the conditional rendering of components based on the state
- * of one or more batched promises. Depending on the state of the promises the component will render one
- * of three yielded components: loading, error, or success.
+ * The purpose of this component is to handle the conditional rendering of components based on the
+ * state of one or more batched promises. Depending on the state of the promises the component will
+ * render one of three yielded components: loading, error, or success.
  *
  * Attributes accepted by this component:
  * --------------------------------------
- * promises           {String|Array|Object} - One or more promises to compute component state from
+ * promises           {Promise|Array|Object}- One or more promises to compute component state from
  * showLastSuccessful {Boolean}             - If true, display the last successful data instead of
  *                                            the loading or error component when fetching new data
+ * forceResolveAll    {Boolean}             - Fulfill `promise` regardless of whether any of the
+ *                                            batched `promises` reject.
+ * onFulfilledData    {Function}            - Callback called when promise is successfully fulfilled
  * noopComponent      {String}              - Component used when not rendering a state
  * yieldComponent     {String}              - Component used when rendering any state by default
- * loadingComponent   {String}              - Override 'yieldComponent' when rendering loading state
- * errorComponent     {String}              - Override 'yieldComponent' when rendering error state
- * successComponent   {String}              - Override 'yieldComponent' when rendering success state
+ * loadingComponent   {String}              - Override `yieldComponent` when rendering loading state
+ * errorComponent     {String}              - Override `yieldComponent` when rendering error state
+ * successComponent   {String}              - Override `yieldComponent` when rendering success state
  *
  * @module  components/tri-state
  */
@@ -32,11 +38,37 @@ export default Component.extend({
   layout,
 
   /**
-   * Cache for the last resolved data to be shown during requests when showLastSuccessful is true
-   * @type {*}
+   * Computes component state based on the promise state and associated data
+   * @param {String} promiseState The derived state of the promises
+   * @param {*} data The associated data
    * @private
    */
-  _lastResolvedData: null,
+  triState: computed('promise.isPending', function () {
+    const canShowLastSuccessful = this.get('showLastSuccessful') && this.get('_lastResolvedData');
+    const willRenderLoadingComponent = this.get('promise.isPending') && !canShowLastSuccessful;
+    const willRenderErrorComponent = this.get('promise.isRejected') &&
+                                     !canShowLastSuccessful &&
+                                     !this.get('forceResolveAll');
+    const willRenderSuccessComponent = this.get('promise.isFulfilled') ||
+                                       (this.get('promise.isPending') && canShowLastSuccessful);
+
+    return {
+      error: {
+        isActive: this.get('promise.isRejected'),
+        component: willRenderErrorComponent ? this.errorComponent : this.noopComponent,
+        data: willRenderErrorComponent ? this.get('promise.reason') : null,
+      },
+      success: {
+        isActive: this.get('promise.isFulfilled'),
+        component: willRenderSuccessComponent ? this.successComponent : this.noopComponent,
+        data: willRenderSuccessComponent ? this.get('_lastResolvedData') : null,
+      },
+      loading: {
+        isActive: this.get('promise.isPending'),
+        component: willRenderLoadingComponent ? this.loadingComponent : this.noopComponent,
+      },
+    };
+  }),
 
   /**
    * Helper function to resolve promises based on how they are provided
@@ -50,10 +82,16 @@ export default Component.extend({
     }
 
     if (Array.isArray(promises)) {
+      if (this.get('forceResolveAll')) {
+        return RSVP.allSettled(promises);
+      }
       return RSVP.all(promises);
     }
 
     if (typeof promises === 'object') {
+      if (this.get('forceResolveAll')) {
+        return RSVP.hashSettled(promises);
+      }
       return RSVP.hash(promises);
     }
 
@@ -61,54 +99,17 @@ export default Component.extend({
   },
 
   /**
-   * Computes promise state after all promises are either resolved or rejected
-   * @return {Array} Array containing the promise state and data associated with the promise state
-   * @private
-   */
-  _computePromiseState() {
-    const promises = this.get('promises');
-
-    return this._resolvePromises(promises).then((data) => {
-      return [STATE.RESOLVED, data];
-    }, (reason) => {
-      return [STATE.REJECTED, reason];
-    });
-  },
-
-  /**
-   * Computes component state based on the promise state and associated data
-   * @param {String} promiseState The derived state of the promises
-   * @param {*} data The associated data
-   * @private
-   */
-  _setComponentState(promiseState, data = this._lastResolvedData) {
-    const canShowLastSuccessful = (this.showLastSuccessful === true) && isPresent(data);
-    const willRenderSuccessComponent = promiseState === STATE.RESOLVED || ((promiseState === STATE.LOADING) && canShowLastSuccessful);
-    const willRenderLoadingComponent = (promiseState === STATE.LOADING) && !canShowLastSuccessful;
-
-    this.set('componentState', {
-      error: {
-        isActive: promiseState === STATE.REJECTED,
-        component: promiseState === STATE.REJECTED ? this.errorComponent : this.noopComponent,
-        data: promiseState === STATE.REJECTED ? data : null,
-      },
-      success: {
-        isActive: promiseState === STATE.RESOLVED,
-        component: willRenderSuccessComponent ? this.successComponent : this.noopComponent,
-        data: willRenderSuccessComponent ? data : null,
-      },
-      loading: {
-        isActive: promiseState === STATE.LOADING,
-        component: willRenderLoadingComponent ? this.loadingComponent : this.noopComponent,
-      },
-    });
-  },
-
-  /**
-   * Set defaults and request data once we have attributes
+   * Set defaults and resolve `promise` once we have attributes
    */
   didReceiveAttrs() {
     this._super(...arguments);
+
+    /**
+     * Cached data from the last fulfilled promise to use when `showLastSuccessful` is true
+     * @type {*}
+     * @private
+     */
+    this._lastResolvedData = this._lastResolvedData || false;
 
     /**
      * One or more actions that each return a promise.
@@ -120,60 +121,73 @@ export default Component.extend({
      * If true, display the last successful data instead of the loading or error state when
      * fetching new data.
      * @type {Boolean}
+     * @default false
      */
     this.showLastSuccessful = this.getWithDefault('showLastSuccessful', false);
 
     /**
+     * Fulfill `promise`, even if one or more batched `promises` fail
+     * @type {Boolean}
+     * @default false
+     */
+    this.forceResolveAll = this.getWithDefault('forceResolveAll', false);
+
+    /**
      * Name of the component to render when any state is inactive.
      * @type {String}
+     * @default 'tri-noop'
      */
     this.noopComponent = this.getWithDefault('noopComponent', 'tri-noop');
 
     /**
      * Name of the component to render when any state is active, unless overridden.
      * @type {String}
+     * @default 'tri-yield'
      */
     this.yieldComponent = this.getWithDefault('yieldComponent', 'tri-yield');
 
     /**
      * Name of the component to render when the error state is active.
      * @type {String}
+     * @default 'tri-yield'
      */
     this.errorComponent = this.getWithDefault('errorComponent', this.yieldComponent);
 
     /**
      * Name of the component to render when the success state is active.
      * @type {String}
+     * @default 'tri-yield'
      */
     this.successComponent = this.getWithDefault('successComponent', this.yieldComponent);
 
     /**
      * Name of the component to render when the loading state is active.
      * @type {String}
+     * @default 'tri-yield'
      */
     this.loadingComponent = this.getWithDefault('loadingComponent', this.yieldComponent);
 
-    // If there are no promises to act upon fallback to last resolved data or exit with no state
-    if (isEmpty(this.promises)) {
-      if (isPresent(this._lastResolvedData) && (this.showLastSuccessful === true)) {
-        this._setComponentState(STATE.RESOLVED);
-      }
-      return this._setComponentState(STATE.NONE);
-    }
+    /**
+     * Callback called when the promise has successfully resolved
+     * @type {Function}
+     * @default noop
+     */
+    this.onFulfilledData = this.getWithDefault('onFulfilledData', noop);
 
-    // If there are some promises assume loading state
-    this._setComponentState(STATE.LOADING);
+    /**
+     * If one or more promises are provided, batch into a single PromiseProxy `promise` object
+     */
+    if (isPresent(this.promises)) {
+      const promise = this._resolvePromises(this.promises);
 
-    // Once the promises settle then cache resolved data and set the final state
-    this._computePromiseState().then(([promiseState, data]) => {
-      if (promiseState === STATE.RESOLVED) {
+      // Create a promise proxy object that is state aware
+      this.set('promise', PromiseObject.create({ promise }));
+
+      // If promise is fulfilled, cache data and trigger callback
+      promise.then((data) => {
         this._lastResolvedData = data;
-        this.sendAction('onResolvedData', data);
-      }
-
-      run.next(() => {
-        this._setComponentState(promiseState, data)
+        this.onFulfilledData(data);
       });
-    });
+    }
   },
 });
